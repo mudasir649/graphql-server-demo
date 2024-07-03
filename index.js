85f35c888c3c0e22470089f717d300c7cfcd3cb7
Mudassar Riaz <82477790+mudasir649@users.noreply.github.com>
2024-07-03 23:01:34 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
-import { typeDefs } from "./schemas";
-import db from "./_db";
+import { typeDefs } from "./schemas.js";
+import db from "./_db.js";
 
 const resolvers = {
     Query: {
         games(){
             return db.games
         },
+        game(_, args){
+            return db.games.find((game) => game.id === args.id)
+        },
         reviews(){
             return db.reviews
         },
         authors(){
             return db.authors
+        },
+        author(_, args){
+            return db.authors.find((author) => author.id === args.id)
+        },
+        review(_, args){
+            console.log(args);
+            return db.reviews.find((review) => review.id === args.id)
         }
     }
 }
