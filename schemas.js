8dce7471182fb8cbc098b45531231a69cfa12d6d
Mudassar Riaz <82477790+mudasir649@users.noreply.github.com>
2024-07-06 23:37:32 +0500

    Update schemas.js

--- schemas.js
+++ schemas.js
@@ -1,37 +1,80 @@
-export const typeDefs = `#graphql
-      type Game {
-            id: ID!
-            title: String!
-            platform: [String!]!
-            reviews: [Review!]
-      }
-      type Review {
-            id: ID!
-            rating: Int!
-            content: String!
-            author: Author!
-            game: Game!
-      }
-      type Author {
-            id: ID!
-            name: String!
-            verified: Boolean!
-            reviews: [Review!]
-      }
-      type Query {
-            games: [Game]
-            game(id: ID!): Game 
-            reviews: [Review]
-            review(id: ID!): Review
-            authors: [Author]
-            author(id: ID!): Author 
-      }
-      type Mutation{
-            addGame(game: AddGameInput): Game
-            deleteGame(id: ID!): [Game]
-      }            
-      input AddGameInput{
-            title: String,
-            platform: [String!]!
-      }  
-`
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schemas.js";
+import db from "./_db.js";
+
+const resolvers = {
+    Query: {
+        games(){
+            return db.games
+        },
+        game(_, args){
+            return db.games.find((game) => game.id === args.id)
+        },
+        reviews(){
+            return db.reviews
+        },
+        authors(){
+            return db.authors
+        },
+        author(_, args){
+            return db.authors.find((author) => author.id === args.id)
+        },
+        review(_, args){
+            console.log(args);
+            return db.reviews.find((review) => review.id === args.id)
+        }
+    },
+    Game:{
+        reviews(parent){
+            return db.reviews.filter((r) => r.game_id === parent.id)
+        }
+    },
+    Author: {
+        reviews(parent){
+            return db.reviews.filter((r) => r.author_id === parent.id)
+        }
+    },
+    Review: {
+        author(parent){
+            return db.authors.find((a) => a.id === parent.author_id)
+        },
+        game(parent){
+            return db.games.find((g) => g.id === parent.game_id)
+        }
+    },
+    Mutation: {
+        deleteGame(_, args){
+            db.games = db.games.filter((g) => g.id !== args.id);
+            return db.games;
+        },
+        addGame(_, args){
+            let game = {
+                ...args.game,
+                id: Math.floor(Math.random() * 10000).toString()
+            }
+            db.games.push(game);
+            return  game;
+        },
+        updateGame(_, args){
+            db.games = db.games.map((g) => {
+                if(g.id === args.id){
+                    return {...g, ...args.edits}
+                }
+            });
+            return db.games;
+        }
+    }
+}
+
+// server-setup
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000}
+});
+
+console.log('Server ready at port: ', 4000);
